ddc7a068168710a92ad1a3e218b0f19658fcf696
James Cleveland <jc@blit.cc>
2015-03-02 14:55:22 +0000

    more es6-y

--- mkconstants.js
+++ mkconstants.js
@@ -1,15 +1,15 @@
-(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.constantify = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.mkconstants = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 "use strict";
 
-var constantify = function (app, obj, prevK) {
+var mkconstants = function (app, obj, prevK) {
   for (var k in obj) {
-    var nextK = prevK ? prevK + "." + k : app + "." + k;
-    if (obj[k] === null) obj[k] = nextK;else constantify(app, obj[k], nextK);
+    var nextK = "" + (prevK ? prevK : app) + "." + k;
+    if (obj[k] === null) obj[k] = nextK;else mkconstants(app, obj[k], nextK);
   }
   return obj;
 };
 
-module.exports = constantify;
+module.exports = mkconstants;
 
 },{}]},{},[1])(1)
 });
--- src/mkconstants.js
+++ src/mkconstants.js
@@ -1,6 +1,6 @@
 const mkconstants = (app, obj, prevK) => {
-  for (var k in obj) {
-    var nextK = prevK ? prevK + '.' + k : app + '.' + k;
+  for (let k in obj) {
+    let nextK = `${prevK ? prevK : app}.${k}`;
     if (obj[k] === null) obj[k] = nextK;
     else mkconstants(app, obj[k], nextK);
   }
