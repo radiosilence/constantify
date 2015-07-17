a5175bf8d8a0d7610e7667d6de17860caa5b525c
James Cleveland <jc@blit.cc>
2015-07-17 17:31:12 +0100

    smaller build and AUTHORS

--- mkconstants.js
+++ mkconstants.js
@@ -1,15 +1,25 @@
-(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.mkconstants = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-"use strict";
-
-var mkconstants = function (app, obj, prevK) {
-  for (var k in obj) {
-    var nextK = "" + (prevK ? prevK : app) + "." + k;
-    if (obj[k] === null) obj[k] = nextK;else if (typeof obj[k] === "object") mkconstants(app, obj[k], nextK);
+(function (global, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define(['exports', 'module'], factory);
+  } else if (typeof exports !== 'undefined' && typeof module !== 'undefined') {
+    factory(exports, module);
+  } else {
+    var mod = {
+      exports: {}
+    };
+    factory(mod.exports, mod);
+    global.mkconstants = mod.exports;
   }
-  return obj;
-};
+})(this, function (exports, module) {
+  'use strict';
 
-module.exports = mkconstants;
+  var mkconstants = function mkconstants(app, obj, prevK) {
+    for (var k in obj) {
+      var nextK = (prevK ? prevK : app) + '.' + k;
+      if (obj[k] === null) obj[k] = nextK;else if (typeof obj[k] === 'object') mkconstants(app, obj[k], nextK);
+    }
+    return obj;
+  };
 
-},{}]},{},[1])(1)
+  module.exports = mkconstants;
 });
